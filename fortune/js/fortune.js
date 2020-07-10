8e0cd4487083598e50e2402168038bda91e9313b
github user name <oikawa.ayacorbeille@gmail.com>
2020-07-10 23:20:47 +0900

    今日の運勢ファイルの編集
    テスト,学習の為

--- fortune/js/fortune.js
+++ fortune/js/fortune.js
@@ -5,24 +5,26 @@
 
   btn.addEventListener('click' , ()=> {
     
-    const n = Math.floor(Math.random() *100);
-    if (n < 20) {
+    const n = Math.floor(Math.random() *10);
+    if (n == 0) {
       btn.textContent = '料理をすると吉♪'; 
-    } else if (n < 30) {
+    } else if (n == 1) {
       btn.textContent = 'おうちで過ごすと吉♪'; 
-    } else if (n < 40) {
+    } else if (n == 2) {
       btn.textContent = '買い物すると吉♪'; 
-    } else if (n < 50) {
+    } else if (n == 3) {
       btn.textContent = '運動すると吉♪'; 
-    } else if (n < 60) {
+    } else if (n == 4) {
       btn.textContent = '友達に会うと吉♪'; 
-    } else if (n < 70) {
+    } else if (n == 5) {
       btn.textContent = '音楽を聞くと吉♪'; 
-    } else if (n < 80) {
+    } else if (n == 6) {
       btn.textContent = 'ストレッチすると吉♪'; 
-    } else if (n < 90) {
+    } else if (n == 7) {
+      btn.textContent = '髪を切ると吉♪'; 
+    } else if (n == 8) {
       btn.textContent = '本を読むと吉♪'; 
-    } else {
+    } else if (n == 9) {
       btn.textContent = '散歩をすると吉♪'; 
     }
     
